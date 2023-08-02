d7239358603afe61ee44942cbfbad7485ba2f038
Anton Gusev <antongusev705@gmail.com>
2023-08-02 10:46:25 +0300

    Задача 64

---
+++ Z1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace RecursionExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Задаем значение N
+            int N = 10;
+
+            Console.WriteLine("Натуральные числа от N до 1:");
+            PrintNaturalNumbers(N);
+        }
+
+        static void PrintNaturalNumbers(int num)
+        {
+            if (num >= 1)
+            {
+                Console.Write(num + " ");
+                PrintNaturalNumbers(num - 1);
+            }
+        }
+    }
+}
+
+
