bf6dfc3185d28130703a95ba5d4d65c93263d172
Anton Gusev <antongusev705@gmail.com>
2023-08-02 10:53:43 +0300

    Задача 66

---
+++ Z3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SumOfNaturalNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int M = 1;
+            int N = 15;
+
+            int sum = CalculateSumOfNaturalNumbers(M, N);
+            Console.WriteLine($"Сумма натуральных чисел от {M} до {N}: {sum}");
+        }
+
+        static int CalculateSumOfNaturalNumbers(int start, int end)
+        {
+            int sum = 0;
+            for (int i = start; i <= end; i++)
+            {
+                sum += i;
+            }
+            return sum;
+        }
+    }
+}
+
